3e721230cf6f551d0e25084231d79c0584aec79d
Shivambisen <shivambisen16@kgpian.iitkgp.ac.in>
2024-11-05 02:16:37 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import './App.css'
+import { TokenLaunchpad } from './components/TokenLaunchpad'
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+
+import {
+    WalletModalProvider,
+    WalletDisconnectButton,
+    WalletMultiButton
+} from '@solana/wallet-adapter-react-ui';
+
+
+// Default styles that can be overridden by your app
+import '@solana/wallet-adapter-react-ui/styles.css';
+
+function App() {
+  return (
+
+    <ConnectionProvider endpoint={"https://api.devnet.solana.com"}>
+            <WalletProvider wallets={[]} autoConnect>
+                <WalletModalProvider>
+                  <div style={{display:"flex" , justifyContent:"space-between"}}>
+                    <WalletMultiButton />
+                    <WalletDisconnectButton />
+                  </div>    
+                      <TokenLaunchpad></TokenLaunchpad>
+
+                </WalletModalProvider>
+            </WalletProvider>
+        </ConnectionProvider>
+    
+  )
+}
+
+export default App
